a3f80242c870438f2961cd4f2bf68c7f412fc2bf
Valerie <valerie.tram.anh.n@gmail.com>
2021-12-02 20:19:41 -0500

    temporary code

--- public/javascript/searchbar.js
+++ public/javascript/searchbar.js
@@ -20,28 +20,17 @@
 //   console.log('API call error:', err.message);
 // });
 
-const searchBar = document.getElementById('searchbar').value;
 
-searchBar.addEventListener('keyup', (e) => {
-  const searchString = e.target.value.toLowerCase();
 
-  const filteredCurrency = cryptocurrency.filter((currency) => {
-      return (
-          currency.toLowerCase().includes(searchString)
-      );
-  });
-  displayCurrency(filteredCurrency);
-});
-
-const loadCharacters = async () => {
+async function searchCrypto () {
   const rp = require('request-promise');
   const requestOptions = {
   method: 'GET',
   uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
   qs: {
     'start': '1',
-    'limit': '1',
-    'sort': 'name'
+    'limit': '100',
+    // 'sort': 'name'
   },
   headers: {
     'X-CMC_PRO_API_KEY': '9256192e-5bec-495f-ac90-d237f0a1f879'
@@ -52,29 +41,45 @@ const loadCharacters = async () => {
 
   rp(requestOptions).then(response => {
     console.log('API call response:', response);
+    const displayCurrency = (responses) => {
+      const htmlString = responses
+          .map((response) => {
+              return `
+              <li class="character">
+              <h2>${data.name}</h2>
+              <p>Symbol: ${data.symbol}</p>
+              <li>Rank: ${data.cmc_rank}</li>
+              <li>Price: ${data.quote.CAD.price}</li>
+              <li>Volume Change: ${data.quote.CAD.volume_change_24H}</li>
+              <li>Market Cap: ${data.quote.CAD.market_cap}</li>
+              </li>
+          `;
+          })
+          .join('');
+     currencyList.innerHTML = htmlString;
+    };
   }).catch((err) => {
     console.log('API call error:', err.message);
   });
+
+  // const displayCurrency = (responses) => {
+  //   const htmlString = responses
+  //       .map((response) => {
+  //           return `
+  //           <li class="character">
+  //           <h2>${data.name}</h2>
+  //           <p>Symbol: ${data.symbol}</p>
+  //           <li>Rank: ${data.cmc_rank}</li>
+  //           <li>Price: ${data.quote.CAD.price}</li>
+  //           <li>Volume Change: ${data.quote.CAD.volume_change_24H}</li>
+  //           <li>Market Cap: ${data.quote.CAD.market_cap}</li>
+  //           </li>
+  //       `;
+  //       })
+  //       .join('');
+  //  currencyList.innerHTML = htmlString;
+  // };
 }
-  
-const displayCurrency = (responses) => {
-  const htmlString = responses
-      .map((response) => {
-          return `
-          <li class="character">
-          <h2>${data.name}</h2>
-          <p>Symbol: ${data.symbol}</p>
-          <li>Rank: ${data.cmc_rank}</li>
-          <li>Price: ${data.quote.CAD.price}</li>
-          <li>Volume Change: ${data.quote.CAD.volume_change_24H}</li>
-          <li>Market Cap: ${data.quote.CAD.market_cap}</li>
-          </li>
-      `;
-      })
-      .join('');
- currencyList.innerHTML = htmlString;
-};
 
-loadCharacters();
-
+document.querySelector('#searchbar').addEventListener('click', searchCrypto)
 
